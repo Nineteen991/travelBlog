a44370ba2d9ef1f4cee848045e3abf9c9f68c142
Nineteen991 <Nineteen99@gmail.com>
2024-04-10 19:09:45 -0600

    adding title sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import HomePage from "./pages/HomePage"
+import './App.sass'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,14 @@
 export default function Hero () {
   return (
     <div className="hero">
-      hero
+      <div className="hero-desc">
+        <h2 className="hero-tagline--left">Weiner Dog Studios Presents...</h2>
+        <h1 className="hero-title">On The Run</h1>
+        <h2 className="hero-tagline--right">...A semi-fictional travel blog</h2>
+      </div>
+      <div className="hero-thumbnail-images">
+
+      </div>
     </div>
   )
 }
